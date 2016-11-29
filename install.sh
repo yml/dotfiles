b1b729736dfe635dff3050b96c1dce82d2e288c3
Yann Malet <yann.malet@gmail.com>
2016-11-29 23:20:08 +0100

    Adding configuration for nvim

--- install.sh
+++ install.sh
@@ -11,10 +11,26 @@ function link_file {
     ln -sf ${source} ${target}
 }
 
+# Link all the files that starts by `_` and create a symlink into $HOME
 for i in _*
 do
-    link_file $i
+    if [ -f $i ]; then
+        link_file $i
+    fi
 done
+
+# Link pip and vim 
+for i in "_pip" "_vim"
+do
+    if [ -d $i ]; then
+        link_file $i
+    fi
+done
+
+# Link nvim
+mkdir -p $HOME/.config/nvim
+ln -sf $(pwd)/_config/nvim/init.vim $HOME/.config/nvim/init.vim
+
 
 mkdir _vim/bundle
 cd _vim/bundle
