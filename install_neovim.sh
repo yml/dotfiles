4d826540729b6062d83652fcdb433f40754382a7
Yann Malet <yann.malet@gmail.com>
2017-10-19 23:28:20 +0200

    neovim: add the system packge for pyhton3-venv

--- install_neovim.sh
+++ install_neovim.sh
@@ -5,4 +5,4 @@ sudo add-apt-repository ppa:neovim-ppa/stable
 sudo apt-get update
 sudo apt-get install neovim
 
-sudo apt-get install python-dev python-pip python3-dev python3-pip
+sudo apt-get install python-dev python-pip python3-dev python3-pip python3-venv
